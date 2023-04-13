99df87da43d0731f6b458c90037fc17900e0caed
maksimbudzko <mbudko@alfabank.ru>
2023-04-13 23:39:24 +0200

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    application
+}
+
+group = "org.nerallan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("MainKt")
+}
